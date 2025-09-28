164f5908aaebf7dc9d903be19b040014923af308
Mykola Shein <qseboy1998@gmail.com>
2025-09-28 11:46:43 +0300

    fix lang

--- src/services/checkLang.js
+++ src/services/checkLang.js
@@ -1,13 +1,15 @@
 import LanguageDetect from "languagedetect";
 
-export const LngDetect = (text, limit) => {
+export const LngDetect = (text, limit = 1) => {
   const lngDetector = new LanguageDetect();
   const results = lngDetector.detect(text, limit);
+
+  if (!results || results.length === 0) return "en";
 
   const [lang, prob] = results[0];
 
   if (prob < 0.2) return "en";
-  if (lang === "ukranian") return "uk";
+  if (lang === "ukrainian") return "uk";
   if (lang === "english") return "en";
   if (lang === "russian") return "ru";
 
